cf2034d77ea787fb367634856f364e061afb37fa
kunalrawal07 <kunalrawal07@gmail.com>
2025-03-08 18:53:13 +0530

    final done again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ 
+  reactStrictMode: true,
+  swcMinify: true,
+  output: process.env.NEXT_PUBLIC_STATIC === "true" ? "export" : undefined, // Use static export if needed
 };
 
 export default nextConfig;
